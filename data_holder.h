56007b4213fc60c7a1d269176dfc2e82c8fdb9c7
Snewar79 <himera5x@yandex.ru>
2020-04-26 20:01:12 +0300

    data_holder chancge to template

--- data_holder.h
+++ data_holder.h
@@ -2,18 +2,37 @@
 #include <vector>
 #include <iostream>
 
-class data_holder{
+#define v3i std::vector<int>
 
+template<class data_type>
+class data_holder{
 private:
-    std::vector<std::vector<std::vector<double>>> data;
+    std::vector<std::vector<std::vector<data_type>>> data;
     int data_size;
 
 public:
-    data_holder(int _size){
+    data_holder(int _size, data_type default_value){
         data_size = _size;
+        data = std::vector<std::vector<std::vector<data_type>>>(data_size, std::vector<std::vector<data_type>>(data_size, std::vector<data_type>(data_size, default_value)));
     }
 
     int size(){
         return data_size;
+    }
+
+    data_type& operator[](std::vector<int> zyx){
+        return data[zyx[0]][zyx[1]][zyx[2]];
+    }
+
+    void print(){
+        for (int z = 0; z < data_size; z++){
+            std::cout << "Layer z = " << z << "\n";
+            for (int y = 0; y < data_size; y++){
+                for (int x = 0; x < data_size; x++){
+                    std::cout << data[x][y][z] << " ";
+                }
+                std::cout << "\n";
+            }
+        }
     }
 };
--- main.cpp
+++ main.cpp
@@ -3,8 +3,15 @@
 
 int main()
 {
+    data_holder<double> data(5, 0);
+ 
+    //data.print();
 
- 
- 
+    data[v3i{0, 0, 0}] = 1.1;
+    data[v3i{0, 2, 1}] = 2.3;
+    data[v3i{3, 0, 2}] = 3.4;
+
+    data.print();
+
     return 0;
 }
