1f444b84e90e856ae6c8b0df69722c8c756c30cf
Snewar79 <himera5x@yandex.ru>
2020-04-27 16:50:49 +0300

    add file for diff system

--- main.cpp
+++ main.cpp
@@ -1,17 +1,11 @@
 
-#include "data_holder.h"
+#include "diff_system.h"
 
 int main()
 {
-    data_holder<double> data(5, 0);
- 
-    //data.print();
+    diff_system data(5);
 
-    data[v3i{0, 0, 0}] = 1.1;
-    data[v3i{0, 2, 1}] = 2.3;
-    data[v3i{3, 0, 2}] = 3.4;
-
-    data.print();
+    
 
     return 0;
 }
